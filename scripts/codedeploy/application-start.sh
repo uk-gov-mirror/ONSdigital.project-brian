6db32919b098f01b61684276d6a78da3eeb17902
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2016-06-29 11:37:53 +0100

    Only bundle/deploy develop

--- scripts/codedeploy/application-start.sh
+++ scripts/codedeploy/application-start.sh
@@ -3,7 +3,8 @@
 ECR_REPOSITORY_URI=
 GIT_COMMIT=
 
-docker run -d --name brian \
-    --net=publishing       \
-    --restart=always       \
-    $ECR_REPOSITORY_URI/brian:$GIT_COMMIT
+docker run -d      \
+  --name=brian     \
+  --net=publishing \
+  --restart=always \
+  $ECR_REPOSITORY_URI/brian:$GIT_COMMIT
