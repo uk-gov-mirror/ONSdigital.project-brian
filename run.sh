737c974404bb824b2f83657e72ed9cad9b20ddf5
Crispioso <crispin.merriman@gmail.com>
2016-10-03 11:32:23 +0100

    Update JDWP port to 8008 to stop conflict with the-train

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8004,server=y,suspend=n"
+export JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8008,server=y,suspend=n"
 export PORT="8083"
 
 # Restolino configuration
@@ -10,7 +10,7 @@ export PACKAGE_PREFIX=com.github.onsdigital.brian.api
 
 #Development: reloadable
 mvn test dependency:copy-dependencies  && \
- 
+
 mvn package && \
 java $JAVA_OPTS \
  -Drestolino.packageprefix=$PACKAGE_PREFIX \
